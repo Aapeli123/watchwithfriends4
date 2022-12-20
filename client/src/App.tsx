19aa91dccbf20dc46d9583c2a7f9b56b269e1c9e
Aapo Harju <aaposharju@gmail.com>
2022-12-20 11:42:54 +0200

    added router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,20 @@
+import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from 'react-router-dom'
 import './App.css'
+import Home from './pages/Home/Home'
 import Room from './pages/Room/Room'
 import SideBar from './ui/SideBar'
 import TopBar from './ui/TopBar'
+
+const router = createBrowserRouter([
+  {
+    path: "/room",
+    element: <Room />,
+  },
+  {
+    path: "/",
+    element: <Home />,
+  },
+]);
 
 function App(): JSX.Element {
   return (
@@ -10,7 +23,7 @@ function App(): JSX.Element {
       <div className='main-content'>
         <SideBar />
         <div className='app-data'>
-          <Room />
+          <RouterProvider router={router} />
         </div>
       </div>
       
---
+++ client/src/pages/Home/Home.tsx
@@ -0,0 +1,10 @@
+import "./Home.css"
+
+const Home = () => {
+    return <div className="home">
+        <h1>Placeholder text</h1>
+        <p>This is the homescreen, it will hopefully include something interesting</p>
+    </div>
+}
+
+export default Home;
