ab8d4edb1b86a13b0d4f758c141b7bd5bbbb1c31
Aapo <aaposharju@gmail.com>
2022-12-21 17:14:34 +0200

    Reworked the top bar to support multiple items and moved the logo around a bit to match the design

--- client/src/ui/SideBar.tsx
+++ client/src/ui/SideBar.tsx
@@ -3,8 +3,8 @@ import "./SideBar.css";
 const SideBar = () => {
     return <>
         <div className="side-bar">
-            <Link to={"/"}><div className="sidebar-top"><h2>Join Room</h2></div></Link>
-            <Link to={"/room/1"}><div className="sidebar-item"><h4>New Room</h4></div></Link>
+            <Link to={"/room/1"}><div className="sidebar-top"><h2>Join Room</h2></div></Link>
+            <Link to={"/room/2"}><div className="sidebar-item"><h4>New Room</h4></div></Link>
             <Link to={"/info"}><div className="sidebar-item"><h4>Info</h4></div></Link>
             <Link to={"/settings"}><div className="sidebar-item"><h4>Settings</h4></div></Link>
 
--- client/src/ui/TopBar.tsx
+++ client/src/ui/TopBar.tsx
@@ -6,12 +6,16 @@ const TopBar = () => {
     return (
         <>
             <div className="top-bar">
+
                 <div className="logo">
-                    <img src={logo}></img>
-                    <h2 id="logo-text-left">Watchwith</h2>
-                    <h2 id="logo-text-right">friends</h2>
+                <Link to={"/"}>
+                    <div className="logo-text-container">
+                        <img src={logo}></img>
+                        <h2 id="logo-text-left">Watchwith</h2>
+                        <h2 id="logo-text-right">friends</h2>
+                    </div>
+                </Link>
                 </div>
-                <div id="link-breaker"></div>
             </div>
         </>
     )
