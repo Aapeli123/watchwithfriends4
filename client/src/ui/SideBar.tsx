1ed5b81915222ce348dcdd8afb2db920aa24ec5b
Aapo <aaposharju@gmail.com>
2022-12-21 17:27:06 +0200

    Added icons and changed top bar logo background color width

--- client/src/ui/SideBar.tsx
+++ client/src/ui/SideBar.tsx
@@ -4,9 +4,15 @@ const SideBar = () => {
     return <>
         <div className="side-bar">
             <Link to={"/room/1"}><div className="sidebar-top"><h2>Join Room</h2></div></Link>
-            <Link to={"/room/2"}><div className="sidebar-item"><h4>New Room</h4></div></Link>
-            <Link to={"/info"}><div className="sidebar-item"><h4>Info</h4></div></Link>
-            <Link to={"/settings"}><div className="sidebar-item"><h4>Settings</h4></div></Link>
+            <Link to={"/room/2"}>
+                <div className="sidebar-item"><h4><span className="material-icons">add</span>New Room</h4></div>
+            </Link>
+            <Link to={"/info"}>
+                <div className="sidebar-item"><h4><span className="material-icons">menu_book</span>Info</h4></div>
+            </Link>
+            <Link to={"/settings"}>
+                <div className="sidebar-item"><h4><span className="material-icons">settings</span>Settings</h4></div>
+            </Link>
 
         </div>
     </>
