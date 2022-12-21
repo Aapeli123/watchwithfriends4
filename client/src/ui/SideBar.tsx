4f29663b95cc4076d9bb481e00e9f75ef85d10e8
Aapo Harju <aaposharju@gmail.com>
2022-12-21 15:30:08 +0200

    restyled the side bar a bit

--- client/src/ui/SideBar.tsx
+++ client/src/ui/SideBar.tsx
@@ -3,7 +3,7 @@ import "./SideBar.css";
 const SideBar = () => {
     return <>
         <div className="side-bar">
-            <Link to={"/"}><div className="sidebar-top"></div></Link>
+            <Link to={"/"}><div className="sidebar-top"><h2>Join Room</h2></div></Link>
             <Link to={"/room/1"}><div className="sidebar-item"><h4>New Room</h4></div></Link>
             <Link to={"/info"}><div className="sidebar-item"><h4>Info</h4></div></Link>
             <Link to={"/settings"}><div className="sidebar-item"><h4>Settings</h4></div></Link>
