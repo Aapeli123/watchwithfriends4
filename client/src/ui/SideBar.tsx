6339376fd50a7c76a7a310b206c83a97c5e8c76d
Aapo <aaposharju@gmail.com>
2022-12-25 22:01:56 +0200

    The sidebar now changes between room mode and normal mode when joining or exiting room

--- client/src/ui/SideBar.tsx
+++ client/src/ui/SideBar.tsx
@@ -1,13 +1,14 @@
 import React from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 import { ServerConn } from "../lib/conn";
 import { RootState } from "../store/store";
-import { enableRoomBar } from "../store/ui";
 import "./SideBar.css";
 const SideBar = (props: {conn: ServerConn}) => {
     const navigate = useNavigate();
     const username = useSelector((state: RootState) => state.pref.username);
+    const roomBar = useSelector(((state: RootState) => state.ui.roomBar));
+    const roomCode = useSelector(((state: RootState) => state.room.roomCode));
     const createRoomClick = async (e: React.MouseEvent<HTMLAnchorElement>) => {
         e.preventDefault();
         try {
@@ -20,7 +21,14 @@ const SideBar = (props: {conn: ServerConn}) => {
         
     }
 
-    return <>
+    return roomBar ? (
+        <>
+            <div className="side-bar">
+                <div className="sidebar-top"><h2>Room code: {roomCode}</h2></div>
+                <Link to={"/"}><div className="sidebar-item"><h4><span className="material-icons">exit_to_app</span>Leave Room</h4></div></Link>
+            </div>
+        </>
+    ) : (<>
         <div className="side-bar">
             <Link to={"/joinroom"}><div className="sidebar-top"><h2>Join Room</h2></div></Link>
             <a href="/createroom" onClick={createRoomClick}>
@@ -34,7 +42,7 @@ const SideBar = (props: {conn: ServerConn}) => {
             </Link>
 
         </div>
-    </>
+    </>)
 }
 
 export default SideBar;
